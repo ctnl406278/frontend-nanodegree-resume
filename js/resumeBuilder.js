bc0919a6ad061a174cff88fa1867ff9783444444
ctnl406278 <ctnl406278@yahoo.com>
2015-08-02 17:03:49 +0200

    Add console output to resumeBuilder.js and nothing to index.html

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,6 @@ This is empty on purpose! Your code to build the resume will go here.
  */
  
  $("#main").append("Dan Str");
+ 
+ var awesomeThoughts = "I am Danstr and I am AWESOME?";
+	console.log(awesomeThoughts);
