fe5f290cce31d805ec6f89005812f61c7524bcff
ctnl406278 <ctnl406278@yahoo.com>
2015-08-03 11:53:54 +0200

    Cleaned index.html and resumeBuilder.js. Added header data -> name and role.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,22 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  
- $("#main").append("Dan Str");
+/* $("#main").append("Dan Str");
+ 
+var email = "Danstr@email.com";
+var newEmail = email.replace("email","mymail");
+console.log(email);
+console.log(newEmail);
+
+var awesomeThoughts = "I am Danstr and I am AWESOME!";
+$("#main").append(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append(funThoughts); */
+
+var name = "Daniel Struwig";
+var role = "Global Technical Trainee";
+
+var formattedName = HTMLheaderName.replace("%data%", name)
+var formattedRole = HTMLheaderRole.replace("%data%", role)
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
